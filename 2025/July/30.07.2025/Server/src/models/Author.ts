1b8826a897324b53a9adb6325a4f15821fd30eb8
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-07-30 09:24:35 +0300

    Refactor Socket.io chat application to improve room management features and enhance user experience. Update server logic for better message handling and optimize client-side code for performance. Revise README to reflect recent changes and provide clearer instructions for users.

---
+++ 2025/July/30.07.2025/Server/src/models/Author.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Schema } from 'mongoose'
+
+interface IAuthor extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string,
+  bio: string,
+  birthYear: number,
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const authorSchema = new Schema<IAuthor>({
+  name: {
+    type: String,
+    require: true, 
+  },
+  bio: String,
+  birthYear: {
+    type: Number,
+    require: true, 
+  },
+},{
+  timestamps: true
+})
+
+const Author = mongoose.model<IAuthor>("Author", authorSchema)
+export default Author;
