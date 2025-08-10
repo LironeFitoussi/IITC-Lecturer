6669313250395217688ceb0c3b87640f06b1e137
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-08-10 10:31:53 +0300

    Refactor author retrieval logic to improve performance and streamline data response. Enhanced controller methods for better handling of virtual books reference in author data.

---
+++ 2025/August/10.08.2025/Server/src/server.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const PORT = 3000
+const app = express()
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Basic Chat Server On"
+    })
+});
+
+io.on('connection', (socket) => {
+    console.log(`User: ${socket.id} connected to our server`);
+    
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is runnig on port ${PORT}`);
+})
