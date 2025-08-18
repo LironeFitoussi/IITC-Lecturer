f078a116a1a4205873da9ddbb1e7bc86e6601b35
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-08-18 10:55:45 +0300

    Implement navigation structure in App component and update HomeScreen background color. Added StatusBar and integrated Home and Details screens for improved user experience.

--- 2025/August/18.08.2025/starting-project-01/App.tsx
+++ 2025/August/18.08.2025/starting-project-01/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import {  } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
 
 type RooStackParamList = {
   Home: undefined;
@@ -11,7 +17,21 @@ const Stack = createNativeStackNavigator<RooStackParamList>()
 
 export default function App() {
   return (
-    
+    <>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home' screenOptions={{
+          headerTitleAlign: 'center'
+        }}>
+          <Stack.Screen name='Home' component={HomeScreen} options={{
+            title: 'בית'
+          }} />
+          <Stack.Screen name="Details" component={DetailsScreen} options={{
+            title: 'פרטים'
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
--- 2025/August/18.08.2025/starting-project-01/screens/HomeScreen.tsx
+++ 2025/August/18.08.2025/starting-project-01/screens/HomeScreen.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#088F8F',
   },
   title: {
     fontSize: 24,
