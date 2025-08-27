ac7da63462d18e219824cadfe48d7f60b45753e4
LironeFitoussi <lironefit@icloud.com>
2025-08-27 09:47:55 +0300

    feat: enhance Cube component with animation using framer-motion

--- 2025/August/27.08.2025/framer-basics/src/components/Cube.tsx
+++ 2025/August/27.08.2025/framer-basics/src/components/Cube.tsx
@@ -1,7 +1,21 @@
+import { motion } from 'framer-motion'
+
 export default function Cube() {
   return (
-    <div className="w-32 h-32 rounded-2xl bg-blue-500 hover:bg-blue-700 flex items-center justify-center text-white font-bold">
+    <motion.div 
+        initial={{ rotate: 0 }}
+        animate={{ rotate: 360 }}
+        transition={{
+            // duration: 2,
+            // ease: 'backInOut',
+            type: 'spring',
+            stiffness: 500,
+            damping: 10,
+            mass: 1,
+        }}
+        className="w-32 h-32 rounded-2xl bg-blue-500 hover:bg-blue-700 flex items-center justify-center text-white font-bold"
+    >
       Cube
-    </div>
+    </motion.div>
   )
 }
