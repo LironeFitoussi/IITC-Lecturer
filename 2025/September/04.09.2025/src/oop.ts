443ef0bf760f9659627b4b3a0ede39927e8691c7
LironeFitoussi <lironefit@icloud.com>
2025-09-04 10:45:15 +0300

    Implement code changes to enhance functionality and improve performance

--- 2025/September/04.09.2025/src/oop.ts
+++ 2025/September/04.09.2025/src/oop.ts
@@ -1,13 +1,45 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
 
 class Car {
-    make: string = "";
-    model: string = "";
-    year: number = 0;
-    gazType: string = "";
-    maxSpeed: number = 0;
+    constructor(
+        public make: string,
+        public model: string,
+        public year: number,
+        public gazType: string,
+        public maxSpeed: number
+    ) { }
+    drive() {
+        console.log(`${this.make} ${this.model} is driving at ${this.maxSpeed}`);
+    }
+    slowDown(speed: number) {
+        if (this.maxSpeed <= speed) {
+            console.log('The Car Stoped');
+            return
+        }
+
+        this.maxSpeed -= speed
+        console.log(`${this.make} ${this.model} is driving now slower at ${this.maxSpeed}`);
+    }
 }
 
-const newCar1 = new Car()
+const newCar1 = new Car('Toyota', 'Corola', 2010, '95', 210);
+const newCar2 = new Car('Volkswagen', 'Scirocco', 2015, '95', 240);
+const newCar3 = new Car('Chevrolet', 'Spark', 2018, '92', 170);
 
-console.log(newCar1);
+newCar1.drive()
+newCar1.slowDown(10)
+newCar1.slowDown(10)
+newCar1.slowDown(10)
+newCar1.slowDown(10)
+newCar1.slowDown(10)
+newCar1.slowDown(50)
+newCar1.slowDown(50)
+newCar1.slowDown(50)
+newCar1.slowDown(50)
+newCar2.slowDown(50)
+newCar3.slowDown(50)
+newCar1.drive()
+
+// console.log(newCar1);
+// console.log(newCar2);
+// console.log(newCar3);
