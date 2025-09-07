4cc477c2a0bc9237dd4d3e1f83a156bebf6c5253
LironeFitoussi <lironefit@icloud.com>
2025-09-07 10:39:15 +0300

    refactor: remove unused NumberUtils class and enhance Utils class functionality

--- 2025/September/07.09.2025/src/oop-generics.ts
+++ 2025/September/07.09.2025/src/oop-generics.ts
@@ -26,18 +26,47 @@
 
 // -----------------------
 
-class NumberUtils {
-    /**
-     * @param nums array of nums
-     * @decription this gets an array of numbers
-     * @returns boolean
-    */
-    static allPositive(nums: number[]): boolean {
-        for (let n of nums) {
-            if ( n <= 0) return false
+// class NumberUtils {
+//     /**
+//      * @param nums array of nums
+//      * @decription this gets an array of numbers
+//      * @returns boolean
+//     */
+//     static allPositive(nums: number[]): boolean {
+//         for (let n of nums) {
+//             if ( n <= 0) return false
+//         }
+//         return true;
+//     }
+// }
+
+// console.log(NumberUtils.allPositive([1, 5, -6]));
+
+
+// ---------------------------
+class Utils {
+    static allPass<T>(arr: T[], cb: (item: T) => boolean): boolean{
+        for ( let el of arr) {
+            if (!cb(el)) return false
         }
-        return true;
+        return true
     }
 }
 
-console.log(NumberUtils.allPositive([1, 5, -6]));
+console.log(Utils.allPass<number>([1,2,3], n => n > 0));
+console.log(Utils.allPass<string>(['a', 'b', 'c'], s => s.length >= 1));
+
+interface User {
+    name: string,
+    age: number
+}
+
+const users: User[] = [
+    { name: "Alice", age: 28 },
+    { name: "Bob", age: 34 },
+    { name: "Charlie", age: 17 },
+    { name: "Diana", age: 30 }
+]
+
+console.log(Utils.allPass<User>(users, u => u.age >= 18));
+
