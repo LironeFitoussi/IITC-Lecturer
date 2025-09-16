c0bf43815ce36796cba1a09034ae5361f7c17a1f
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-09-16 11:32:53 +0300

    sdfsdf

--- 2025/September/16.09.2025/joke-app/client/src/main.tsx
+++ 2025/September/16.09.2025/joke-app/client/src/main.tsx
@@ -8,6 +8,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Create a client
 const queryClient = new QueryClient();
@@ -15,6 +16,8 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   // Provide the client to your App
   <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+
     <App />
   </QueryClientProvider>
 );
