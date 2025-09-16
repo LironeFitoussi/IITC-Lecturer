0339fb73c6c8914bce1e86cd0d580696acb080bc
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-09-16 11:54:24 +0300

    feat: enhance useAddJoke hook with query invalidation on success
    - Integrated useQueryClient to invalidate 'jokes' query upon successful joke creation.
    - Improved the mutation handling for adding jokes to ensure the UI reflects the latest data.

--- 2025/September/16.09.2025/joke-app/client/src/hooks/useAddJoke.tsx
+++ 2025/September/16.09.2025/joke-app/client/src/hooks/useAddJoke.tsx
@@ -1,8 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { createJoke } from "../services/jokes.service";
+import { useQueryClient } from "@tanstack/react-query";
+
 
 const useAddJoke = () => {
-    const mutation = useMutation({ mutationFn: createJoke})
+    const queryClient = useQueryClient()
+    const mutation = useMutation({ 
+        mutationFn: createJoke,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['jokes']})
+        }
+    })
     return mutation
 }
 
